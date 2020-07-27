53031c31527f39b95dc0f670c72b4884bf108795
Phoomparin Mano <phoomparin@gmail.com>
2020-07-28 03:06:34 +0700

    feat: add planning tests for sample budget

--- libs/plan/tests/plan.test.ts
+++ libs/plan/tests/plan.test.ts
@@ -2,13 +2,20 @@ import * as fs from 'fs'
 
 import {parseFinancialPlan, calculateFinancialPlan} from '../src'
 
-const SampleFile = fs.readFileSync('./libs/plan/tests/my.budget', 'utf-8')
-
 describe('Financial Planner', () => {
-  it('should be able to parse', () => {
-    const financialPlan = parseFinancialPlan(SampleFile)
-    const calculations = calculateFinancialPlan(financialPlan, 130000)
+  const SampleFile = fs.readFileSync('./libs/plan/tests/sample.budget', 'utf-8')
+  const SamplePlan = parseFinancialPlan(SampleFile)
 
-    console.log(calculations)
+  it('should be able to parse financial plan', () => {
+    expect(SamplePlan.budget.length).toBe(9)
+    expect(SamplePlan.plan.length).toBe(5)
+    expect(SamplePlan.investment.length).toBe(5)
+  })
+
+  it('should be able to calculate financial plan', () => {
+    const {allocations} = calculateFinancialPlan(SamplePlan, 130000)
+
+    expect(allocations.BTS).toBe(1450)
+    expect(allocations.Motorcycle).toBe(600)
   })
 })
