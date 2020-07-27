89bee7d5c9d862fb30266da63bf474e4c76e825a
Phoomparin Mano <phoomparin@gmail.com>
2020-07-28 03:18:56 +0700

    fix: parse plan in every unit to measure execution time

--- libs/plan/tests/plan.test.ts
+++ libs/plan/tests/plan.test.ts
@@ -4,16 +4,18 @@ import {parseFinancialPlan, calculateFinancialPlan} from '../src'
 
 describe('Financial Planner', () => {
   const SampleFile = fs.readFileSync('./libs/plan/tests/sample.budget', 'utf-8')
-  const SamplePlan = parseFinancialPlan(SampleFile)
 
   it('should be able to parse financial plan', () => {
-    expect(SamplePlan.budget.length).toBe(9)
-    expect(SamplePlan.plan.length).toBe(5)
-    expect(SamplePlan.investment.length).toBe(5)
+    const plan = parseFinancialPlan(SampleFile)
+
+    expect(plan.budget.length).toBe(9)
+    expect(plan.plan.length).toBe(5)
+    expect(plan.investment.length).toBe(5)
   })
 
   it('should be able to calculate financial plan', () => {
-    const {allocations} = calculateFinancialPlan(SamplePlan, 130000)
+    const plan = parseFinancialPlan(SampleFile)
+    const {allocations} = calculateFinancialPlan(plan, 130000)
 
     expect(allocations.BTS).toBe(1450)
     expect(allocations.Motorcycle).toBe(600)
