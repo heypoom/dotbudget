e4e970c156c5eeb659f1203d1aad34ea92c17e5c
Phoomparin Mano <phoomparin@gmail.com>
2020-07-28 22:28:13 +0700

    fix: alter overspent tag position on mobile

--- apps/web/src/budget-card/CardTitle.tsx
+++ apps/web/src/budget-card/CardTitle.tsx
@@ -4,12 +4,19 @@ import styled from '@emotion/styled'
 
 const OverspentTag = styled.div`
   right: -18px;
+
   --transform-rotate: 30deg;
+
+  @media screen and (max-width: 640px) {
+    left: -40px;
+    right: auto;
+    --transform-rotate: -30deg;
+  }
 `
 
 export function BudgetCardTitle(props: BudgetCardProps) {
   const baseClass =
-    'relative rounded-tl-lg rounded-tr-lg px-5 py-1 text-white text-center md:text-left shadow-md z-10'
+    'rounded-tl-lg rounded-tr-lg px-5 py-1 text-white text-center md:text-left shadow-md z-10'
 
   const isOverBudget = props.spent > props.allocated
 
@@ -18,7 +25,7 @@ export function BudgetCardTitle(props: BudgetCardProps) {
       <div className={c(baseClass, 'bg-gradient-red shadow-md-pink')}>
         <span>{props.title}</span>
 
-        <OverspentTag className="absolute top-0 bg-white transform px-3 py-1 rounded-full shadow-md-pink">
+        <OverspentTag className="absolute top-0 bottom-auto bg-white transform px-3 py-1 rounded-full shadow-md-pink">
           <strong className="font-bold text-sm bg-gradient-red text-gradient text-red">
             Overspent!
           </strong>
