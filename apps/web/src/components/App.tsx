a909f50f87aea65708be0dd79094537ae8df0f41
Phoomparin Mano <phoomparin@gmail.com>
2020-07-28 18:46:04 +0700

    feat: improve border and shadow styling

--- apps/web/src/components/App.tsx
+++ apps/web/src/components/App.tsx
@@ -18,7 +18,7 @@ interface BudgetCardProps {
 
 function BudgetCardTitle(props: BudgetCardProps) {
   const baseClass =
-    'rounded-tl-lg rounded-tr-lg px-5 py-1 text-white text-center md:text-left shadow-md'
+    'rounded-tl-lg rounded-tr-lg px-5 py-1 text-white text-center md:text-left shadow-md z-10'
 
   if (props.isOverBudget) {
     return (
@@ -39,7 +39,7 @@ const BudgetCard = (props: BudgetCardProps) => (
   <div className="mx-auto flex flex-col rounded-lg shadow-xl w-full">
     <BudgetCardTitle {...props} />
 
-    <div className="p-4 px-6">
+    <div className="p-4 px-6 bg-white rounded-bl-lg rounded-br-lg">
       <div className="text-3xl">{categoryEmoji[props.category]}</div>
 
       <div className="text-4xl">{props.amount || 0}</div>
@@ -48,7 +48,7 @@ const BudgetCard = (props: BudgetCardProps) => (
 )
 
 export const App = () => (
-  <main>
+  <main className="bg-gray-100 h-screen">
     <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 p-6">
       {categories.map(category => (
         <BudgetCard category={category} amount={50} key={category} />
