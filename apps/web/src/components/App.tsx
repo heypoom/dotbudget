65df59d2b3a473302c09edaa26758dbb37384e10
Phoomparin Mano <phoomparin@gmail.com>
2020-07-29 00:54:05 +0700

    feat: adjust grid size and gap

--- apps/web/src/components/App.tsx
+++ apps/web/src/components/App.tsx
@@ -42,7 +42,7 @@ const budgets = transformBudget(calculated.monthlyBudgets)
 
 export const App = () => (
   <main className="bg-gray-200 h-screen">
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4 p-6">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-5 p-6">
       {budgets.map(budget => (
         <BudgetCard key={budget.title} {...budget} />
       ))}
