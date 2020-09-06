f0387aab03279d4f4c4c7b0fbe5c27909e272dff
Phoomparin Mano <phoomparin@gmail.com>
2020-09-07 05:54:41 +0700

    feat: improve documentation on types

--- libs/plan/src/@types/index.ts
+++ libs/plan/src/@types/index.ts
@@ -4,13 +4,18 @@ import {frequencies, jars} from '../constants'
  * Plan blueprints are the schema of how you manage your finances.
  */
 export interface PlanBlueprint {
+  /** Jars define how your money is being partitioned. (e.g. necessity, security) */
   jars: Jars
-  budget: Budget[]
-  investment: Investments
+
+  /** Investments define where you'll be investing your money in. (e.g. gold, stocks, bonds) */
+  investments: Investments
+
+  /** Budgets define how much budget you've allocated to each spending category. */
+  budgets: Budget[]
 }
 
 /**
-  Jars define how your money is being partitioned.
+  Jars define how your money is being partitioned. (e.g. necessity, security)
 
   @example
   jars:
@@ -24,7 +29,7 @@ export type Jars = Record<Jar, number>
 export type Jar = typeof jars[number]
 
 /**
-  Investments define how the rest of your money will be invested.
+  Investments define where you'll be investing your money in. (e.g. gold, stocks, bonds)
 
   @example
   investments:
