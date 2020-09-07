32564c3aa8efce487d0155e12d6cb9bdf8538f74
Phoomparin Mano <phoomparin@gmail.com>
2020-09-07 13:18:03 +0700

    feat: add persistence

--- apps/web/store/index.ts
+++ apps/web/store/index.ts
@@ -1,11 +1,12 @@
 import {createContext} from 'react'
 import {createStoreon} from 'storeon'
 import {customContext} from 'storeon/react'
+import {persistState} from '@storeon/localstorage'
 
 import {PlanModule} from './plan'
 import {SpendingModule} from './spending'
 
-export const store = createStoreon([PlanModule, SpendingModule])
+export const store = createStoreon([PlanModule, SpendingModule, persistState()])
 
 export const StoreContext = createContext(store)
 export const useStore = customContext(StoreContext)
