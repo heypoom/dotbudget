106fd4ba46c6d22f080dab29c622c91b76accd9b
Phoomparin Mano <phoomparin@gmail.com>
2020-09-07 08:29:43 +0700

    fix: widen grid

--- apps/web/dashboard/index.tsx
+++ apps/web/dashboard/index.tsx
@@ -50,7 +50,7 @@ const BudgetGrid = () => {
   )
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-5 p-6">
+    <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 md:gap-5 p-6">
       {budgets.map(budget => (
         <BudgetCard key={budget.name} {...budget} />
       ))}
