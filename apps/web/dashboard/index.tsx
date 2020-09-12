0c21b0c5ec189d00d1b468b15ef118b76390e3e3
Phoomparin Mano <phoomparin@gmail.com>
2020-09-12 07:46:20 +0700

    refactor: extract desktop ui

--- apps/web/dashboard/index.tsx
+++ apps/web/dashboard/index.tsx
@@ -6,22 +6,26 @@ import {InputModeIndicator} from './InputModeIndicator'
 import {Editor} from '../editor'
 import {CommandPalette} from '../command-palette'
 
+const DesktopUI = () => (
+  <div className="flex">
+    <div className="w-8/12 relative">
+      <BudgetGrid />
+      <CommandPalette />
+
+      <InputModeIndicator />
+    </div>
+
+    <div className="w-4/12">
+      <Editor />
+    </div>
+  </div>
+)
+
 export const Dashboard = () => {
   return (
     <main className="bg-darker h-screen">
       <div>
-        <div className="flex">
-          <div className="w-8/12 relative">
-            <BudgetGrid />
-            <CommandPalette />
-
-            <InputModeIndicator />
-          </div>
-
-          <div className="w-4/12">
-            <Editor />
-          </div>
-        </div>
+        <DesktopUI />
       </div>
     </main>
   )
